b3a3a10aa4b2a7dd83d500875f914665c365bf71
Arthur Warmling <arthur@consolidesuamarca.com.br>
2021-01-29 14:22:39 -0300

    Add Axios;

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
-import router from './router';
-import store from './store';
-import App from './App';
-
-import './assets/css/tailwindcss.css';
+import router from '@/router';
+import store from '@/store';
+import App from '@/App.vue';
+import '@/plugins/vee-validate.js'
+import '@/plugins/axios.js'
+import '@/assets/css/tailwindcss.css';
 
 Vue.config.productionTip = false;
 
